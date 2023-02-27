be2a5afaacf57afb1196d294e4f33edf168924ca
Connor Nicholls <CRNicholls20@gmail.com>
2023-02-27 15:54:48 -0600

    properly generated transaction list, however need a new name database to pull from as old one did not work, also need to figure out why a new line is being generated every time we write a line

--- populate_database.py
+++ populate_database.py
@@ -2,18 +2,21 @@ import csv
 import os
 import random
 import names
+from datetime import datetime
+from datetime import timedelta
 
 os.chdir('data')
 
 seed = random.seed(int)
 
-#menu item list
+#menu item list (format is [PK,Name,Price,Ingredient_Amount,Type])
 menu_item = []
 with open('menu_item.csv', newline='') as menuItem:
-    menuItemReader = csv.reader(menuItem)
+    menuItemReader = csv.reader(menuItem, delimiter=',')
     for row in menuItemReader:
         menu_item.append(row)
 
+#print(menu_item)
 #create writers and open files
 transaction = open('transaction.csv', 'w')
 transactionItem = open('transaction_item.csv', 'w')
@@ -25,20 +28,33 @@ transactionItemWriter = csv.writer(transactionItem)
 inventoryWriter = csv.writer(inventory)
 orderWriter = csv.writer(order)
 
+timeInc = timedelta(days=1)
+
 #for 1 day in 1 year
+dt = datetime(2022,1,1,1,1,1,0) #first day/time
 for i in range(0,365):
     transID = 0
-    #generate transactions
-    for j in range(0,275):
+    #generate transactions (format is [ID, employee_ID, name, price, time])
+    maxDailyTransactions = 275
+    for j in range(0,maxDailyTransactions):
         trans = []
         trans.append(transID)
         trans.append(random.randrange(0,2))
-        trans.append(names.get_full_name())
+        #trans.append(names.get_full_name())
+        trans.append("karen")
         numberItems = random.randrange(1,4)
+        costTransaction = 0
+        #pick random line from menu_item, add cost to transaction, decrement inventory
         for k in range(numberItems):
-            #pick random line from menu_item, add cost to transaction, decrement inventory
+            item = menu_item[random.randrange(1,len(menu_item))]
+            costTransaction += float(item[2]) * float(item[3])
+            #TODO: subtract from inventory
+        trans.append(costTransaction)
         #timestamp transaction
-        #add to csv
+        trans.append(dt)
+        dt += timeInc/maxDailyTransactions
+        #add to csv 
+        transactionWriter.writerow(trans)
     #account for gamedays
 
 
@@ -48,3 +64,7 @@ for i in range(0,365):
     #place order if needed at end of day
 
 #close files
+transaction.close()
+transactionItem.close()
+inventory.close()
+order.close()
