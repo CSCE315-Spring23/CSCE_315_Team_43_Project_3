64fa157ab829c7e43d6a2f2001eeb2b78cc1e468
clint2j <94955448+clint2j@users.noreply.github.com>
2023-03-06 12:41:02 -0600

    Working on integrating GUI

--- smook/src/main/java/com/team43/app/frontend/server/ServerFrame.java
+++ smook/src/main/java/com/team43/app/frontend/server/ServerFrame.java
@@ -5,18 +5,28 @@ import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.Container;
+import java.awt.Color;
 
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.Font;
 
-public class ServerFrame extends JFrame {
+import java.util.ArrayList;
+
+public class ServerFrame extends JFrame implements ActionListener {
     JButton b1;
     JPanel contentPane;
+    JPanel item_type;
+    JPanel Transaction;
+    JPanel Items;
+    JLabel lblNewLabel;
+    JLabel lblNewLabel_2;
+    ArrayList<JButton> item_t;
+    ArrayList<JButton> smoothies;
 
     public ServerFrame(int width, int height) {
         System.out.println("Hola");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         setSize(width, height);
 
@@ -27,29 +37,46 @@ public class ServerFrame extends JFrame {
         contentPane.setLayout(null);
 
         //Initial Layout
-        JPanel Transaction = new JPanel();
+        Transaction = new JPanel();
 		Transaction.setBounds(0, 0, width/2, height);
 		contentPane.add(Transaction);
-		Transaction.setLayout(null);
+		Transaction.setLayout(new BoxLayout(Transaction, BoxLayout.PAGE_AXIS));
 
-        JPanel Items = new JPanel();
+        Items = new JPanel();
 		Items.setBounds(width/2, 0, width/2, height);
 		contentPane.add(Items);
-		Items.setLayout(new GridLayout(0, 2, 0, 0));
+		Items.setLayout(new BoxLayout(Items, BoxLayout.PAGE_AXIS));
 
         //First labels
-        JLabel lblNewLabel_2 = new JLabel("Order");
+        lblNewLabel_2 = new JLabel("Order");
         lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(41, 11, width/2,height/11);
+		lblNewLabel_2.setBounds(41, 11, width/2,height/12);
 		Transaction.add(lblNewLabel_2);
 
-        JLabel lblNewLabel = new JLabel("Items");
+        lblNewLabel = new JLabel("Items");
         lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(10, 9, 448, 14);
+		lblNewLabel.setBounds(41, 11, width/2, height/12);
 		Items.add(lblNewLabel);
 
-        //
+        /*To Do: 
+        **Add 2 panels (One for item type and one for item. Fill those panels using arrayLists). 
+        **Remove grid layout from item panel since the previously mentioned panels will be below it and have that grid layout. 
+        **Maybe use a different (vertical) layout. But grid is probably best(for now).
+    */
+        setItemType();
+        //item_type.setBounds(width/2, height/12, width/2, 10*height/12);
+        //item_type.setBorder(new LineBorder(new Color(200,0,0),3));
+        // Items.add(item_type);
+    }
+    //Add a function to add an item for controller
+    public void setItemType() {
+        item_type = new JPanel(new GridLayout(0, 1, 0, 0));
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+
     }
 }
