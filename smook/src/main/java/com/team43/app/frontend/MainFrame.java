5979bd4bacc2d1e4474903e4c1e33371a942cbc6
johndpjr <johndpowell02@gmail.com>
2023-03-07 11:30:09 -0600

    add controller

--- smook/src/main/java/com/team43/app/frontend/MainFrame.java
+++ smook/src/main/java/com/team43/app/frontend/MainFrame.java
@@ -27,6 +27,7 @@ public class MainFrame extends JFrame {
         loginPanel.setVisible(false);
         revalidate();
         repaint();
+        add(panel);
         panel.setVisible(true);
     }
 
