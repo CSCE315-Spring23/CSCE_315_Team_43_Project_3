26b79c509ed22db3c61b45e3ecadedaf38eb72ae
Krish Chhabra <kachhabra02@tamu.edu>
2023-03-07 19:31:44 -0600

    finished backend interface class and most db interaction functions

--- smook/src/main/java/com/team43/app/backend/server/jdbcpostgreSQL.java
+++ smook/src/main/java/com/team43/app/backend/server/jdbcpostgreSQL.java
@@ -1,9 +1,9 @@
 package com.team43.app.backend.server;
 
+import com.team43.app.backend.server.*; // important
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import com.team43.app.backend.server.*; // important
 
 public class jdbcpostgreSQL {
 
@@ -14,14 +14,16 @@ public class jdbcpostgreSQL {
   Connection conn = null;
   String teamNumber = "team_43";
   String dbName = "csce315331_" + teamNumber;
-  String dbConnectionString = "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
-  
+  String dbConnectionString =
+      "jdbc:postgresql://csce-315-db.engr.tamu.edu/" + dbName;
+
   public jdbcpostgreSQL() {
     dbSetup myCredentials = new dbSetup();
 
     // Connecting to the database
     try {
-      conn = DriverManager.getConnection(dbConnectionString, dbSetup.user, dbSetup.pswd);
+      conn = DriverManager.getConnection(dbConnectionString, dbSetup.user,
+                                         dbSetup.pswd);
     } catch (Exception e) {
       e.printStackTrace();
       System.err.println(e.getClass().getName() + ": " + e.getMessage());
@@ -29,38 +31,133 @@ public class jdbcpostgreSQL {
     }
   }
 
-  // Add necessary DB functions
-
-  // REMOVE
-  public List<List<String>> view_inventory() {
-    List<List<String>> table = new ArrayList<List<String>>();
+  // returns list of categories in the database
+  public ArrayList<String> getCategories() {
+    ArrayList<String> categories = new ArrayList<String>();
     try {
       // create a statement object
       Statement stmt = conn.createStatement();
 
       // Running a query
-      String sqlStatement = "SELECT * FROM inventory";
+      String sqlStatement = "SELECT DISTINCT type FROM menu_item";
 
       // send statement to DBMS
       ResultSet result = stmt.executeQuery(sqlStatement);
 
-
       // OUTPUT
       while (result.next()) {
-        List<String> elements = new ArrayList<String>();
-        elements.add(result.getString("inventory_id"));
-        elements.add(result.getString("name"));
-        elements.add(result.getString("price"));
-        elements.add(result.getString("quantity"));
-        elements.add(result.getString("measurement_type"));
-        table.add(elements);
+        categories.add(result.getString("type"));
       }
     } catch (Exception e) {
       e.printStackTrace();
       System.err.println(e.getClass().getName() + ": " + e.getMessage());
       System.exit(0);
     }
-    return table;
+    return categories;
+  }
+
+  // returns list of items in given category
+  public ArrayList<String> getItemsInCategory(String category) {
+    ArrayList<String> items = new ArrayList<String>();
+    try {
+      // create a statement object
+      Statement stmt = conn.createStatement();
+
+      // Running a query
+      String sqlStatement =
+          "SELECT name FROM menu_item WHERE type='" + category + "'";
+
+      // send statement to DBMS
+      ResultSet result = stmt.executeQuery(sqlStatement);
+
+      // OUTPUT
+      while (result.next()) {
+        items.add(result.getString("name"));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.err.println(e.getClass().getName() + ": " + e.getMessage());
+      System.exit(0);
+    }
+    return items;
+  }
+
+  // returns all ingredients in the database
+  public ArrayList<String> getAllIngredients() {
+    ArrayList<String> items = new ArrayList<String>();
+    try {
+      // create a statement object
+      Statement stmt = conn.createStatement();
+
+      // Running a query
+      String sqlStatement =
+          "SELECT name FROM menu_item WHERE name NOT IN ('Small Cup', 'Medium Cup', 'Large Cup', 'Small Straw', 'Large Straw', 'Assorted Snacks', 'Dummy Item')";
+
+      // send statement to DBMS
+      ResultSet result = stmt.executeQuery(sqlStatement);
+
+      // OUTPUT
+      while (result.next()) {
+        items.add(result.getString("name"));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.err.println(e.getClass().getName() + ": " + e.getMessage());
+      System.exit(0);
+    }
+    return items;
+  }
+
+  // TODO
+  public MenuItem getMenuItem(String menu_item) {}
+
+  // returns number of different items in the inventory
+  public int getNumInventoryItems() {
+    int num_items = 0;
+    try {
+      // create a statement object
+      Statement stmt = conn.createStatement();
+
+      // Running a query
+      String sqlStatement = "SELECT COUNT(inventory_id) FROM inventory";
+
+      // send statement to DBMS
+      ResultSet result = stmt.executeQuery(sqlStatement);
+
+      // OUTPUT
+      num_items = result.getInt("count");
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.err.println(e.getClass().getName() + ": " + e.getMessage());
+      System.exit(0);
+    }
+    return num_items;
+  }
+
+  // TODO
+  public void writeTransactionData(Transaction trans) {}
+
+  // returns id of most recent transaction
+  public int getLastTransactionID() {
+    int trans_id = 0;
+    try {
+      // create a statement object
+      Statement stmt = conn.createStatement();
+
+      // Running a query
+      String sqlStatement = "SELECT MAX(transaction_id) FROM transaction";
+
+      // send statement to DBMS
+      ResultSet result = stmt.executeQuery(sqlStatement);
+
+      // OUTPUT
+      trans_id = result.getInt("max");
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.err.println(e.getClass().getName() + ": " + e.getMessage());
+      System.exit(0);
+    }
+    return trans_id;
   }
 
   public boolean close_connection() {
@@ -73,4 +170,4 @@ public class jdbcpostgreSQL {
     } // end try catch
     return true;
   }
-}// end Class
+} // end Class
