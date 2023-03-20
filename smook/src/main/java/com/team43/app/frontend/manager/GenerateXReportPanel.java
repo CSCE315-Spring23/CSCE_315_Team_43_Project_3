e9d544b8e0f4349842f25366b743b4f2852a2525
johndpjr <johndpowell02@gmail.com>
2023-03-20 15:57:39 -0500

    create Sales, X, & Z manager panels

---
+++ smook/src/main/java/com/team43/app/frontend/manager/GenerateXReportPanel.java
@@ -0,0 +1,12 @@
+package com.team43.app.frontend.manager;
+
+import javax.swing.JPanel;
+
+import com.team43.app.frontend.Controller;
+
+public class GenerateXReportPanel extends JPanel {
+    Controller controller;
+    GenerateXReportPanel(Controller controller) {
+
+    }
+}
---
+++ smook/src/main/java/com/team43/app/frontend/manager/GenerateZReportPanel.java
@@ -0,0 +1,12 @@
+package com.team43.app.frontend.manager;
+
+import javax.swing.JPanel;
+
+import com.team43.app.frontend.Controller;
+
+public class GenerateZReportPanel extends JPanel {
+    Controller controller;
+    GenerateZReportPanel(Controller controller) {
+
+    }
+}
