cde230106797bc2b7fd33ccb78bd810479827d03
Connor Nicholls <CRNicholls20@gmail.com>
2023-04-03 09:48:44 -0500

    set up project structure

---
+++ smook/src/main/java/com/team43/project3/smook/service/SmookServiceImpl.java
@@ -0,0 +1,9 @@
+package com.team43.project3.smook.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SmookServiceImpl implements SmookService{
+    
+}
