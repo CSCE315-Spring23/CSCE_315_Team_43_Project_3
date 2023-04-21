efb68eceea18ce8ac2b3d6460ae1a53fcdc4a2a4
johndpjr <johndpowell02@gmail.com>
2023-04-21 09:40:25 -0500

    show weather on menu board

--- frontend/smook_app/src/App.vue
+++ frontend/smook_app/src/App.vue
@@ -1,16 +1,6 @@
 <script setup>
 import {RouterView } from 'vue-router'
-function httpGet(url) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-}
-let apikey = import.meta.env.VITE_OPENWEATHERMAP_API_KEY;
-let resp = httpGet(`https://api.openweathermap.org/data/2.5/forecast?lat=30.6212&lon=-96.3404&appid=${apikey}&units=imperial&cnt=1`);
-const weather = JSON.parse(resp);
-let forecast = weather.list[0];
-console.log(`${forecast.weather[0].description}, ${forecast.main.temp}°`);
+
 </script>
 
 <template>
--- frontend/smook_app/src/views/MenuBoard.vue
+++ frontend/smook_app/src/views/MenuBoard.vue
@@ -1,7 +1,24 @@
 <script setup>
 import Heading from '../components/Heading.vue';
+let apikey = import.meta.env.VITE_OPENWEATHERMAP_API_KEY;
+
+function httpGet(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+function getWeather() {
+    let resp = httpGet(`https://api.openweathermap.org/data/2.5/forecast?lat=30.6212&lon=-96.3404&appid=${apikey}&units=imperial&cnt=1`);
+    const weather = JSON.parse(resp);
+    let forecast = weather.list[0];
+    return `${forecast.weather[0].description}, ${forecast.main.temp}°`
+}
+
 </script>
 <template>
     <Heading />
-    <h1>Manu</h1>
+    <h1>Menu</h1>
+    <h2>{{ getWeather() }}</h2>
 </template>
