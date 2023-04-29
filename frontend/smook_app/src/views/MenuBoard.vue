d427ceb0ee97382257f83d5c0d280a04a2ce3b75
johndpjr <johndpowell02@gmail.com>
2023-04-29 14:10:40 -0500

    fix setInterval undesired behavior & add categories

--- frontend/smook_app/src/views/MenuBoard.vue
+++ frontend/smook_app/src/views/MenuBoard.vue
@@ -1,7 +1,11 @@
 <script setup>
 import Heading from '../components/Heading.vue';
+import SmookControllerApi from '../classes/api/SmookControllerApi';
+import { ref, onMounted, onUnmounted } from 'vue';
 const apikey = import.meta.env.VITE_OPENWEATHERMAP_API_KEY;
 const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=30.6212&lon=-96.3404&appid=${apikey}&units=imperial&cnt=1`
+
+let client = new SmookControllerApi();
 
 function httpGet(url) {
     var xmlHttp = new XMLHttpRequest();
@@ -9,9 +13,6 @@ function httpGet(url) {
     xmlHttp.send(null);
     return xmlHttp.responseText;
 }
-
-// Update the weather every minute
-setInterval(setWeather, 60 * 1000);
 
 function getWeather() {
     let resp = httpGet(weatherUrl);
@@ -24,6 +25,30 @@ function setWeather() {
     document.getElementById("weatherMsg").textContent = getWeather();
 }
 
+let curIntervalId = null;
+
+onMounted(() => {
+    // Update the weather every minute
+    curIntervalId = setInterval(setWeather, 60 * 1000);
+})
+
+onUnmounted(() => {
+    // Stop auto-updating weather
+    clearInterval(curIntervalId);
+})
+
+const categories = ref([]);
+
+async function getCategories() {
+    client.sendCategories((error, data, resp) => {
+        if (data) {
+            categories.value = data;
+        }
+    })
+}
+
+getCategories();
+
 </script>
 <template>
     <Heading />
@@ -32,7 +57,9 @@ function setWeather() {
             <h1>Menu</h1>
             <h2 id="weatherMsg">{{ getWeather() }}</h2>
             <div class="menu-items">
-                <!-- Placeholder for menu items -->
+                <ul>
+                    <li v-for="cat in categories">{{ cat }}</li>
+                </ul>
             </div>
         </div>
     </main>
