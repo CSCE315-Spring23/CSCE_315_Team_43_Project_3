678082598a4acbd93665033295674e3c273d2e71
clint2j <94955448+clint2j@users.noreply.github.com>
2023-04-30 18:25:20 -0500

    Server view basically works. I implemented subsitutions fully and the ability to delete errent purchases. There is still an issue with pricing (server side issue) and there is still many bugs and needed appereance changes I need to get to.

---
+++ frontend/smook_app/src/components/server/Category.vue
@@ -0,0 +1,83 @@
+<script setup>
+
+import { reactive } from 'vue'
+import Smoothie from './Smoothie.vue'
+import axios from 'axios'
+const props = defineProps({
+  catName: {
+    type: String,
+    required: true
+  }
+})
+
+const smoothies = reactive([]);
+async function getSmoothies() {
+    axios.get('/api/items', { params: { category: props.catName } })
+  .then(response => {
+    console.log("Response" + response.data);
+    const drinks = response.data;
+    for (let i = 0; i<drinks.length; i++){
+        //console.log(drinks[drinks.length-i-1]);
+        smoothies.push(drinks[drinks.length-i-1]);
+    }
+    smoothies.sort((a, b) => {
+  if (a.startsWith('The ')) {
+    a = a.substr(4);
+  }
+  if (b.startsWith('The ')) {
+    b = b.substr(4);
+  }
+  return a.localeCompare(b);
+});
+    //console.log(categories); // Prints the array of strings to the console
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+getSmoothies();
+</script>
+<template>
+    <div id="cat">
+        <div class="divider">
+            <h2>{{ catName }}</h2>
+        </div>
+        <div class="items">
+            <!-- <div v-for="smoothie in smoothies" :key="smoothie.name">
+                <h3>{{ smoothie.name }}</h3>
+                <img :src="smoothie.image" alt="Ayo">
+            </div> -->
+            <Smoothie
+            v-for="smoothie in smoothies"
+            :key="smoothie"
+            :item="smoothie"
+            />
+        </div>
+    </div>
+</template>
+<style scoped>
+#cat {
+    width: 15%;
+}
+    .divider {
+        width: 100%;
+        background-color: lightcoral;
+        margin-top: 12px;
+        margin-bottom: 12px;
+    }
+    .divider:first-of-type {
+        margin-top: 0;
+    }
+    .divider h2 {
+        text-align: center;
+    }
+    .items {
+        display: flex;
+        flex-direction: column;
+        /* flex-wrap: wrap; */
+        /* justify-content: space-between; */
+        overflow: visible;
+    }
+
+</style>
--- frontend/smook_app/src/views/ServerView.vue
+++ frontend/smook_app/src/views/ServerView.vue
@@ -1,7 +1,85 @@
 <script setup>
 import Heading from '../components/Heading.vue';
+
+import { reactive } from 'vue';
+import Category from '../components/server/Category.vue'
+import Transaction from '../components/server/Transaction.vue'
+import Custom from '../components/server/Custom.vue'
+import axios from 'axios';
+
+
+const itemTypes = reactive([]);
+async function getCatagories() {
+  axios.get('/api/category', {
+})
+  .then(response => {
+    const categories = response.data;
+    for (let i = 0; i<categories.length; i++){
+      if (categories[categories.length-i-1] == "Other")
+      continue
+      if (categories[categories.length-i-1] == "Enhance")
+      continue
+      itemTypes.push(categories[categories.length-i-1]);
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+getCatagories();
 </script>
+
 <template>
-    <Heading />
-    <h1>Server</h1>
+      <Heading />
+      <!-- <button style="z-index: 10000;" @click="getCatagories">Hello</button> -->
+  <main>
+    <div id="main">
+        <div id="orderPanel">
+            <Category 
+                v-for="item in itemTypes"
+                :key="item"
+                :catName="item"
+            />
+        </div>
+        <Transaction/>
+    </div>
+  </main>
+  <Custom/>
+  <cartButton />
 </template>
+<style scoped>
+main {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top:0;
+  left:0;
+}
+#main {
+  height: 89%;
+  width: 100%;
+  position: absolute;
+  top: 75px;
+  display: flex;
+}
+#main header {
+    height: 35%;
+    background-color: lightblue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+#main header h1 {
+    font-size: 100px;
+    text-align: center;
+}
+#orderPanel {
+    border: 4px black solid;
+    /* width: 75%; */
+    display: flex;
+    justify-content: center;
+}
+#cartViewServer {
+    width: 40%;
+}
+</style>
