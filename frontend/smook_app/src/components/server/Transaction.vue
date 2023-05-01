01490cfd69abe1a34816351d0d4bb31a979828e8
clint2j <94955448+clint2j@users.noreply.github.com>
2023-05-01 00:28:52 -0500

    Server view is almost complete with complete ease of use and functionality. Accessability needs to be checked.

--- frontend/smook_app/src/components/server/Transaction.vue
+++ frontend/smook_app/src/components/server/Transaction.vue
@@ -1,5 +1,114 @@
+<script setup>
+import {useCartStore} from '@/stores/CartStore';
+import { ref } from 'vue';
+const cartStore = useCartStore();
+const subtotal = ref(0.0);
+const tax = ref(0.0);
+const total = ref(0.0);
+function totaler() {
+  subtotal.value = 0.0;
+  tax.value = 0.0;
+  total.value = 0.0;
+    for(let i = 0; i<cartStore.cart.length; i++){
+        let item = cartStore.cart[i];
+        subtotal.value += item.price;
+    }
+    tax.value = subtotal.value*.0825;
+    total.value = tax.value + subtotal.value;
+    subtotal.value = subtotal.value.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+          })
+    tax.value = tax.value.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD'
+        })
+    total.value = total.value.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+          })
+}
+totaler();
+function submit() {
+    let name = prompt("What's your name?");
+    if (name != null){
+      cartStore.send(name);
+      cartStore.$reset();
+      router.replace('/customer');
+    }
+}
+</script>
+
 <template>
-    <div id="cartViewServer">
-        
+    <Heading />
+  <div id="main">
+    <h1>Current Order</h1>
+    <div id="cartItems">
+        <div v-for="item in cartStore.cart">
+            <h3>{{ item.name }} ({{ item.size }})- {{ item.price.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+          }) }}</h3>
+          <button @click="cartStore.removeItem(item);totaler()" id="xButton">x</button>
+        </div>
     </div>
+    <div id="price">
+        <h5>Subtotal: {{ subtotal }}</h5>
+        <h5>Tax: {{ tax }}</h5>
+        <h5>Total: {{ total }}</h5>
+    </div>
+    <button @click="submit">Pay and Order</button>
+  </div>
 </template>
+
+<style scoped>
+#xButton {
+    display: inline;
+}
+h3 {
+  display: inline;
+}
+#main {
+  top: 75px;
+  padding: 12px;
+}
+#cartItems {
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin: 20px 0;
+}
+#cartItems h3 {
+  margin: 5px 0;
+  font-size: 18px;
+}
+#cartItems h3 span:nth-child(1) {
+  margin-right: 10px;
+}
+#price {
+  margin-top: 20px;
+}
+#price h5 {
+  margin: 5px 0;
+  font-size: 16px;
+  font-weight: normal;
+}
+#price h5:first-child {
+  font-weight: bold;
+}
+#main button {
+  background-color: #008CBA;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px;
+  font-size: 18px;
+  cursor: pointer;
+}
+#main button:hover {
+  opacity: 0.8;
+}
+#main button:focus {
+  outline: none;
+  box-shadow: 0 0 0 2px #008CBA;
+}
+</style>
--- frontend/smook_app/src/views/ServerView.vue
+++ frontend/smook_app/src/views/ServerView.vue
@@ -1,50 +1,142 @@
 <script setup>
 import Heading from '../components/Heading.vue';
-
-import { reactive } from 'vue';
-import Category from '../components/server/Category.vue'
-import Transaction from '../components/server/Transaction.vue'
-import Custom from '../components/server/Custom.vue'
+import { reactive, ref } from 'vue';
+import Transaction from '../components/server/Transaction.vue';
+import Custom from '../components/server/Custom.vue';
 import axios from 'axios';
-
-
+import { useItemStore } from '@/stores/CurrentItem';
+import {useCartStore} from '@/stores/CartStore';
+const itemStore = useItemStore();
+const cartStore = useCartStore();
 const itemTypes = reactive([]);
+const activeOne = ref(0);
+const smoothiesDisplayed = reactive([]);
+const addOn = ref(false)
+const opac = ref(1.0);
 async function getCatagories() {
-  axios.get('/api/category', {
-})
-  .then(response => {
+  try {
+    const response = await axios.get('/api/category');
     const categories = response.data;
-    for (let i = 0; i<categories.length; i++){
-      if (categories[categories.length-i-1] == "Other")
-      continue
-      if (categories[categories.length-i-1] == "Enhance")
-      continue
-      itemTypes.push(categories[categories.length-i-1]);
+    for (let i = 0; i < categories.length; i++) {
+      if (categories[categories.length - i - 1] == 'Other' || categories[categories.length - i - 1] == 'Enhance') {
+        continue;
+      }
+      itemTypes.push({ category: categories[categories.length - i - 1], active: 'notSelected' });
     }
-  })
-  .catch(error => {
+  } catch (error) {
     console.error(error);
-  });
+  }
+  itemTypes[0].active = 'selected';
+  switcher(0);
+}
+
+async function getSmoothies(cat) {
+  try {
+    const response = await axios.get('/api/items', { params: { category: cat } });
+    const smoothies = response.data;
+    for (let i = 0; i < smoothies.length; i++) {
+      smoothiesDisplayed.push(smoothies[i]);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  smoothiesDisplayed.sort((a, b) => {
+  if (a.startsWith('The ')) {
+    a = a.substr(4);
+  }
+  if (b.startsWith('The ')) {
+    b = b.substr(4);
+  }
+  return a.localeCompare(b);
+});
+}
+
+function switcher(i) {
+  itemTypes[activeOne.value].active = 'notSelected';
+  itemTypes[i].active = 'selected';
+  smoothiesDisplayed.splice(0, smoothiesDisplayed.length);
+  getSmoothies(itemTypes[i].category);
+  activeOne.value = i;
+}
+
+function switchRight(i) {
+  if (i < itemTypes.length - 1) {
+    switcher(i + 1);
+  }
+}
+
+function switchLeft(i) {
+  if (i > 0) {
+    switcher(i - 1);
+  }
+}
+function clicked(smoothie) {
+  opac.value = .2;
+  itemStore.changeName(smoothie);
+  addOn.value = true;
+}
+function close() {
+  opac.value = 1;
+  addOn.value = false;
+  itemStore.$reset();
+}
+function cart() {
+  if(itemStore.size =='none')
+  return
+  let smoothie = {
+    name: itemStore.name,
+    size: itemStore.size,
+    ingredients: itemStore.ingredients,
+    price: itemStore.price
+}
+cartStore.addItem(smoothie);
+close();
+console.log(cartStore.cart.length);
 }
 getCatagories();
-</script>
 
+</script>
 <template>
-      <Heading />
-      <!-- <button style="z-index: 10000;" @click="getCatagories">Hello</button> -->
+  <Heading />
   <main>
-    <div id="main">
-        <div id="orderPanel">
-            <Category 
-                v-for="item in itemTypes"
-                :key="item"
-                :catName="item"
-            />
+    <div id="main" :style="{opacity: opac}">
+      <div id="orderPanel">
+        <div id="typePicker">
+          <div
+            v-for="(itemType, i) in itemTypes"
+            :key="'type-' + i"
+            @click="switcher(i)"
+            :class="{'selectedd': itemType.active === 'selected'}"
+            tabindex="0"
+            @keydown.enter="switcher(i)"
+            @keydown.arrowright.prevent="switchRight(i)"
+            @keydown.arrowleft.prevent="switchLeft(i)"
+            role="button"
+            aria-label="Select item type"
+          >
+            {{ itemType.category }}
+          </div>
         </div>
-        <Transaction/>
+        <div id="smoothiePicker">
+          <div
+            v-for="(smoothie, i) in smoothiesDisplayed"
+            :key="'smoothie-' + i"
+            class="smook"
+            @click="clicked(smoothie)"
+          >
+            {{ smoothie }}
+          </div>
+        </div>
+        <h3>OrderPanel</h3>
+      </div>
+      <Transaction/>
     </div>
+    <Custom 
+    v-if="addOn==true"
+    @eventt="close"
+    @carted="cart"
+    />
   </main>
-  <Custom/>
   <cartButton />
 </template>
 <style scoped>
@@ -74,12 +166,61 @@ main {
     text-align: center;
 }
 #orderPanel {
-    border: 4px black solid;
-    /* width: 75%; */
-    display: flex;
-    justify-content: center;
+  width: 74%;
+  height: 100%;
+  border-right: 4px black solid;
 }
 #cartViewServer {
-    width: 40%;
+    width: 400px;
+    background-color: green;
+}
+#typePicker {
+  display: flex;
+  justify-content: center;
+  flex: 1;
+  border-bottom: 2px solid #FF6B6B;
+}
+#typePicker div {
+  flex-grow: 1;
+  text-align: center;
+  cursor: pointer;
+  font-size: 23px;
+  border-right: 2px dashed #FF6B6B;
+  padding: 17px 20px;
+  transition: background-color .5s;
+}
+#typePicker div:hover {
+  background-color: rgb(199, 225, 241);
+}
+#typePicker div:last-of-type {
+  border: 0;
+}
+.selectedd  {
+  background-color: lightskyblue;
+  text-decoration: underline;
+}
+#smoothiePicker {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  height: 73%;
+}
+.smook {
+  border: 3px black solid;
+  border-radius: 20px;
+  padding: 20px;
+  font-size: 21px;
+  margin: 5px;
+  cursor: pointer;
+}
+.smook:hover {
+  background-color: #FF6B6B;
+}
+#orderPanel h3 {
+  bottom: 0;
+  font-size: 30px;
+  text-align: center;
+  margin-top: 20px
 }
 </style>
