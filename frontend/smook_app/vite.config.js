43bf13d05392a50cfa04458fc5d1aa055b61b40f
josefmun9902 <josefmun9902@gmail.com>
2023-05-02 23:48:18 -0500

    push for connor

--- frontend/smook_app/vite.config.js
+++ frontend/smook_app/vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from 'vite-plugin-vuetify'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
