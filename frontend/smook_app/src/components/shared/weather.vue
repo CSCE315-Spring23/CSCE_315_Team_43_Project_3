30241b20a9fdf7448a0c5974064615fb4d37c80f
clint2j <94955448+clint2j@users.noreply.github.com>
2023-05-07 23:31:42 -0500

    Nice menu board

---
+++ frontend/smook_app/src/components/shared/weather.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps(['msg']);
+
+</script>
+
+<template>
+    <div id="weather_API">
+        <h4>{{ msg }}</h4>
+    </div>
+</template>
+
+<style scoped>
+    #weather_API {
+        border: 2px black solid;
+        padding: 5px;
+        width: 100px;
+        position: absolute;
+        bottom: 0;
+    }
+</style>
--- frontend/smook_app/src/views/MenuBoard.vue
+++ frontend/smook_app/src/views/MenuBoard.vue
@@ -2,6 +2,7 @@
 import Heading from '../components/Heading.vue';
 import SmookControllerApi from '../classes/api/SmookControllerApi';
 import { ref, onMounted, onUnmounted } from 'vue';
+import weather from '../components/shared/weather.vue';
 const apikey = "77d7af00a098e224eca5ce31cafde321";
 const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=30.6212&lon=-96.3404&appid=${apikey}&units=imperial&cnt=1`
 
@@ -83,44 +84,91 @@ getCategories();
 <template>
     <Heading />
     <main>
-        <div id="main">
-            <h1>Menu</h1>
-            <h2 id="weatherMsg">{{ weatherMsg }}</h2>
-            <div class="menu-items">
-                <div v-for="[cat, _] in menuData" class="category-container">
-                    <h2>{{ cat }}</h2>
-                    <ul>
-                        <li v-for="menuItem in menuData.get(cat)">{{ menuItem.name }} - $<b>{{ menuItem.price }}</b></li>
-                    </ul>
-                </div>
-            </div>
+      <div id="main">
+        <h1>Smoothie King Menu</h1>
+        <!-- <h2 id="weatherMsg">{{ weatherMsg }}</h2> -->
+        <weather 
+        :msg=weatherMsg
+        />
+        <div class="menu-items">
+          <div v-for="[cat, items] in menuData" class="category-container">
+            <h3>{{ cat }}</h3>
+            <ul>
+              <li v-for="item in items" class="menu-item">
+                <div class="item-name">{{ item.name }}</div>
+                <div class="item-price">$ {{ item.price }}</div>
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
     </main>
-</template>
-
-<style scoped>
-main {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top:0;
-  left:0;
+  </template>
+  
+  <style scoped>
+    main {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  
+    #main {
+      height: 89%;
+      width: 100%;
+      position: absolute;
+      top: 75px;
+    }
+    h1 {
+        text-align: center;
+    }
+    h3 {
+        font-weight: 600;
+        text-align: center;
+    }
+    .menu-items {
+        /* display: flex;
+        flex-direction: column;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  align-content: flex-start */
+  display: flex;
+  flex-wrap: wrap;
+  height: 100vh;
 }
-#main {
-  /* margin-top: 80px;
-  height: 100%; */
-  height: 89%;
-  width: 100%;
-  position: absolute;
-  top: 75px;
-  /* height: 1000px; */
-}
-
-.menu-items {
-    display: flex;
-    width: 100%;
-    justify-content: space-evenly;
-    flex-direction: row;
-    align-items: flex-start;
-}
-</style>
+  
+    .category-container {
+        /* flex-basis: 30%;
+  max-width: 300px;
+  margin: 0 10px 40px;
+  padding: 15px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  background-color: #f9f9f9; */
+  flex: 1 0 auto;
+  min-width: 200px;
+  height: auto;
+  background-color: #f2f2f2;
+  margin: 10px;
+    }
+  
+    .menu-item {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      font-size: 18px;
+      line-height: 1.5;
+      margin-bottom: 10px;
+      border-bottom: 1px black dotted;
+    }
+  
+    .item-name {
+    }
+  
+    .item-price {
+      margin-left: 5px;
+      white-space: nowrap;
+    }
+  </style>
+  
